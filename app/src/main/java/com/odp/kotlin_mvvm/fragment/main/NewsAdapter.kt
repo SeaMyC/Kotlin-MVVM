93e420f9eff57d9abc9c9c7d74c1ed77f31c4ef0
chenhaihai <zkykchan.azr@dairy-farm.com.hkhk>
2020-09-17 15:18:27 +0800

    fixed NewsAdapter logic

--- app/src/main/java/com/odp/kotlin_mvvm/fragment/main/NewsAdapter.kt
+++ app/src/main/java/com/odp/kotlin_mvvm/fragment/main/NewsAdapter.kt
@@ -1,5 +1,6 @@
 package com.odp.kotlin_mvvm.fragment.main
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,8 +13,9 @@ import com.bumptech.glide.Glide
 import com.odp.kotlin_mvvm.R
 import com.odp.kotlin_mvvm.bean.BannerEntity
 import com.odp.kotlin_mvvm.databinding.ItemFragementSportBinding
-import com.odp.kotlin_mvvm.util.LoadMoreAdapter
+import com.odp.kotlin_mvvm.databinding.ItemFragementSportSimpleBinding
 import com.odp.kotlin_mvvm.util.ScreenUtils
+import kotlinx.android.synthetic.main.item_fragement_sport_simple.view.*
 import kotlin.math.roundToInt
 
 /**
@@ -21,97 +23,156 @@ import kotlin.math.roundToInt
  * @time   2020/8/18 10:15
  * @des
  **/
-class NewsAdapter : LoadMoreAdapter<BannerEntity>() {
-    private lateinit var wealListener: IWealItemListener
+class NewsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var datas = mutableListOf<BannerEntity>()
+    lateinit var wealListener: IWealItemListener
+    private val MULTIPLE_IMAGE: Int = 0
+    private val SIMPLE_IMAGE: Int = 1
 
-
-    override fun getSubItemCount(): Int {
+    override fun getItemCount(): Int {
         return datas.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is NormalHolder) {
+            holder.bindData(position)
+        } else {
+            val simpleHolder = holder as SimpleHolder
+            simpleHolder.bindData(position)
+        }
     }
 
-    override fun setNormalHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
-        return NormalHolder(
+    override fun getItemViewType(position: Int): Int {
+        return if (!datas.isNullOrEmpty() && datas[position].pic2.isNullOrEmpty()) SIMPLE_IMAGE
+        else MULTIPLE_IMAGE
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return if (viewType == MULTIPLE_IMAGE) NormalHolder(
             DataBindingUtil.inflate(
-                LayoutInflater.from(parent!!.context),
+                LayoutInflater.from(parent.context),
                 R.layout.item_fragement_sport, parent, false
             )
         )
-    }
-
-    override fun onBindNormalViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
-        val normalHolder = holder as NormalHolder
-        normalHolder.bindData(position, datas, wealListener)
+        else
+            SimpleHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.item_fragement_sport_simple, parent, false
+                )
+            )
     }
 
     fun setDataList(bean: MutableList<BannerEntity>?) {
         if (bean != null) {
-            datas = bean
+            this.datas = bean
             notifyDataSetChanged()
         }
     }
 
-    private class NormalHolder(itemView: ViewDataBinding) : RecyclerView.ViewHolder(itemView.root) {
+    inner class NormalHolder(itemView: ViewDataBinding) : RecyclerView.ViewHolder(itemView.root) {
         private val itemBinding: ItemFragementSportBinding = itemView as ItemFragementSportBinding
 
-        fun bindData(position: Int, list: List<BannerEntity>, listener: IWealItemListener) {
-            val bannerEntity = list[position]
-            itemBinding.llImage.removeAllViews()
+        @SuppressLint("SetTextI18n")
+        fun bindData(position: Int) {
+            if (!datas.isNullOrEmpty()) {
+                val bannerEntity = datas[position]
+                itemBinding.tvTitle.text = bannerEntity.title
+                itemBinding.tvDesc.text =
+                    StringBuffer().append(bannerEntity.authorName).append("  ")
+                        .append(bannerEntity.date)
+                itemBinding.tvDelete.setOnClickListener {
+                    datas.removeAt(position)
+                    notifyDataSetChanged()
+                }
 
-            itemBinding.tvTitle.text = bannerEntity.title
-            val screenUtils = ScreenUtils();
-            val screenWidth = screenUtils.getScreenWidth(itemView.context)
-            val width = (screenWidth - screenUtils.dpToPx(itemView.context, 40f)) / 3
+                itemBinding.llImage.removeAllViews()
+                val screenUtils = ScreenUtils()
+                val screenWidth = screenUtils.getScreenWidth(itemView.context)
+                val width = (screenWidth - screenUtils.dpToPx(itemView.context, 40f)) / 3
 
-            val ivOne = ImageView(itemView.context)
-            val layoutParamsOne = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            layoutParamsOne.marginEnd = 15
-            layoutParamsOne.width= width.roundToInt()
-            ivOne.layoutParams = layoutParamsOne
-            Glide.with(itemView.context)
-                .load(bannerEntity.pic1)
-                .placeholder(R.drawable.img_default_meizi)
-                .error(R.drawable.load_err)
-                .into(ivOne)
-            itemBinding.llImage.addView(ivOne)
+                val ivOne = ImageView(itemView.context)
+                val layoutParamsOne = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                layoutParamsOne.marginEnd = 15
+                layoutParamsOne.width = width.roundToInt()
+                ivOne.layoutParams = layoutParamsOne
+                Glide.with(itemView.context)
+                    .load(bannerEntity.pic1)
+                    .placeholder(R.drawable.img_default_meizi)
+                    .error(R.drawable.load_err)
+                    .into(ivOne)
+                itemBinding.llImage.addView(ivOne)
 
-            val ivTwo = ImageView(itemView.context)
-            val layoutParamsTwo = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            layoutParamsTwo.marginEnd = 15
-            layoutParamsTwo.width= width.roundToInt()
-            ivTwo.layoutParams = layoutParamsTwo
-            Glide.with(itemView.context)
-                .load(bannerEntity.pic2)
-                .placeholder(R.drawable.img_default_meizi)
-                .error(R.drawable.load_err)
-                .into(ivTwo)
-            itemBinding.llImage.addView(ivTwo)
+                val ivTwo = ImageView(itemView.context)
+                val layoutParamsTwo = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                layoutParamsTwo.marginEnd = 15
+                layoutParamsTwo.width = width.roundToInt()
+                ivTwo.layoutParams = layoutParamsTwo
+                Glide.with(itemView.context)
+                    .load(bannerEntity.pic2)
+                    .placeholder(R.drawable.img_default_meizi)
+                    .error(R.drawable.load_err)
+                    .into(ivTwo)
+                itemBinding.llImage.addView(ivTwo)
 
-            val ivThree = ImageView(itemView.context)
-            val layoutParamsThree = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            ivThree.layoutParams = layoutParamsThree
-            layoutParamsThree.width= width.roundToInt()
-            Glide.with(itemView.context)
-                .load(bannerEntity.pic3)
-                .placeholder(R.drawable.img_default_meizi)
-                .error(R.drawable.load_err)
-                .into(ivThree)
-            itemBinding.llImage.addView(ivThree)
+                if (!bannerEntity.pic3.isNullOrEmpty()) {
+                    val ivThree = ImageView(itemView.context)
+                    val layoutParamsThree = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                    ivThree.layoutParams = layoutParamsThree
+                    layoutParamsThree.width = width.roundToInt()
+                    Glide.with(itemView.context)
+                        .load(bannerEntity.pic3)
+                        .placeholder(R.drawable.img_default_meizi)
+                        .error(R.drawable.load_err)
+                        .into(ivThree)
+                    itemBinding.llImage.addView(ivThree)
+                }
 
 
-            itemBinding.rlView.setOnClickListener {
-                listener.onItemListener(bannerEntity.url, itemBinding.rlView)
+                itemBinding.rlView.setOnClickListener {
+                    wealListener.onItemListener(bannerEntity.url, itemBinding.rlView)
+                }
+            }
+        }
+    }
+
+    inner class SimpleHolder(itemView: ViewDataBinding) : RecyclerView.ViewHolder(itemView.root) {
+        private val itemBinding: ItemFragementSportSimpleBinding =
+            itemView as ItemFragementSportSimpleBinding
+
+        @SuppressLint("SetTextI18n")
+        fun bindData(position: Int) {
+            if (!datas.isNullOrEmpty()) {
+                val bannerEntity = datas[position]
+                itemBinding.tvTitle.text = bannerEntity.title
+                itemBinding.tvDesc.text =
+                    StringBuffer().append(bannerEntity.authorName).append("  ")
+                        .append(bannerEntity.date)
+                itemBinding.tvDelete.setOnClickListener {
+                    datas.removeAt(position)
+                    notifyDataSetChanged()
+                }
+
+                Glide.with(itemView.context)
+                    .load(bannerEntity.pic1)
+                    .placeholder(R.drawable.img_default_meizi)
+                    .error(R.drawable.load_err)
+                    .into(itemView.iv_pic)
+
+
+                itemBinding.rlView.setOnClickListener {
+                    wealListener.onItemListener(bannerEntity.url, itemBinding.rlView)
+                }
             }
         }
     }
@@ -124,6 +185,4 @@ class NewsAdapter : LoadMoreAdapter<BannerEntity>() {
     interface IWealItemListener {
         fun onItemListener(str: String?, view: View?)
     }
-
-
 }
